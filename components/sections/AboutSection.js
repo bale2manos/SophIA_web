6b2d47693811aaca3610afe41301564e4d4587c9
Javier Pallarés <90272125+bale2manos@users.noreply.github.com>
2025-06-30 15:40:22 +0200

    Refactor sections into components

---
+++ components/sections/AboutSection.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css';
+
+export default function AboutSection() {
+  return (
+    <section id="about" className={`${styles.section} ${styles.about}`}>
+      <h1>Quiénes somos</h1>
+    </section>
+  );
+}
