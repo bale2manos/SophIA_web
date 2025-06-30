649706b82b70a614a8031c847d58f09a3de5403f
Javier Pallarés <90272125+bale2manos@users.noreply.github.com>
2025-06-30 15:40:36 +0200

    Refactor sections into components

---
+++ components/sections/HomeSection.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css';
+
+export default function HomeSection() {
+  return (
+    <section id="home" className={`${styles.section} ${styles.home}`}>
+      <h1>Home</h1>
+    </section>
+  );
+}
